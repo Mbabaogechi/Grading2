7e3340626f69b32e4bfeee7f7e0d40d24afdb0fa
mbaba ogechi <mbabaogechi@gmail.com>
2019-02-19 10:57:55 +0000

    my assignment on Quadratic

---
+++ QuadraticEqn.java
@@ -0,0 +1,13 @@
+public class QuadraticEqn{
+
+    public static void main(String[] args) {
+
+        int a = 2, b = 4, c = 2;
+        double temp1, root1, root2;
+        temp1 = Math.sqrt(b * b - 4 * a * c);
+        root1 = ((-b + temp1) / (2 * a));
+        root2 =((-b - temp1) / (2*a));
+        System.out.println("the root of the quadratic equation is" + root1);
+    }
+}
+
